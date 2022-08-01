449c61f642e6f92ef2351facbc3e6a178ed07c70
BISWAYAN MEHRA <108832092+Biswayan-Mehra@users.noreply.github.com>
2022-08-01 16:26:08 +0530

    Ex. 3(a) BISWAYAN MEHRA URK21CS1004
    ERROR CORRECTED WITH LINE 33.

--- Main.py
+++ Main.py
@@ -30,7 +30,7 @@ class Solution:
         """
         # Write your code here
         if self.top == -1:
-            return True:
+            return True
         else:
             return False
 
